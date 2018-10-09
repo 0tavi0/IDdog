5ec73e98c3af28ce0be5c3a678e400031b51ba62
Otavio <otavio.le@gmail.com>
2018-10-08 21:39:48 -0300

    criando alertDialog para tratar erros, troca do Glide pelo Fresco,

--- app/src/main/java/com/otavio/iddog/Adapters/AdapterURLs.java
+++ app/src/main/java/com/otavio/iddog/Adapters/AdapterURLs.java
@@ -1,19 +1,19 @@
-package com.otavio.iddog.Adapters;
+package com.otavio.iddog.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-import com.bumptech.glide.Glide;
-import com.otavio.iddog.DogDetails;
-import com.otavio.iddog.Models.Dogs;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.otavio.iddog.activitys.DogDetailsActivity;
 import com.otavio.iddog.R;
 
 import java.util.List;
@@ -21,6 +21,7 @@ import java.util.List;
 public class AdapterURLs extends RecyclerView.Adapter<AdapterURLs.ViewHolder> {
     List<String> listaURL;
     Context context;
+    ProgressBar progressBar;
 
     public AdapterURLs(List<String> listaURL, Context context) {
         this.listaURL = listaURL;
@@ -30,20 +31,21 @@ public class AdapterURLs extends RecyclerView.Adapter<AdapterURLs.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Fresco.initialize(context);
+
         View view = LayoutInflater.from(context).inflate(R.layout.item_dog,parent,false);
         return new AdapterURLs.ViewHolder(view);     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
 
-        Glide.with(context)
-                .load(listaURL.get(position))
-                .into(holder.ima);
+        Uri imageUri = Uri.parse(listaURL.get(position));
+        holder.draweeView.setImageURI(imageUri);
 
         holder.ima.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(context, DogDetails.class);
+                Intent it = new Intent(context, DogDetailsActivity.class);
                 it.putExtra("url", listaURL.get(position));
                 context.startActivity(it);
             }
@@ -57,9 +59,14 @@ public class AdapterURLs extends RecyclerView.Adapter<AdapterURLs.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView ima;
+        ProgressBar progressBar;
+        SimpleDraweeView draweeView;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            progressBar = itemView.findViewById(R.id.progress);
             ima = itemView.findViewById(R.id.img_dog);
+            draweeView = itemView.findViewById(R.id.img_dog);
         }
     }
 }
